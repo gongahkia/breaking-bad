ea06ec216c6b1bb909cbd3b53c1a568464db5477
gongahkia <angryapplegravy@gmail.com>
2024-09-07 13:47:50 +0800

    added

---
+++ main.py
@@ -0,0 +1,10 @@
+# ~ main function ~
+    # to create ./lib/ and put all black scholz functions there
+    # put all execution code here
+
+def main():
+    print("breaking bad")
+    return
+
+if __name__ == "__main__":
+    main()
